2435a37eb139ab77d6bd94b0b4e73a84af591d39
Andres Jaanson <A_Jaanson@hotmail.com>
2018-10-16 15:17:50 +0200

    grant rights to user for all fututre tables

--- src/main/resources/createDB.sql
+++ src/main/resources/createDB.sql
@@ -4,24 +4,17 @@ CREATE USER application_account WITH password 'password';
 
 CREATE SCHEMA data;
 
-CREATE TABLE data.challenge (
-    id        	BIGSERIAL PRIMARY KEY,
-    user_id     BIGINT NOT NULL,
-    name       	VARCHAR(255) NOT NULL,
-    structure	BYTEA NOT NULL,
-    created     TIMESTAMP
-);
-
-CREATE TABLE data.submission (
-	id        		BIGSERIAL PRIMARY KEY,
-	user_id     	BIGINT NOT NULL,
-	challenge_id 	BIGINT NOT NULL,
-	structure		BYTEA NOT NULL,
-	created     	TIMESTAMP
-);
-
 GRANT USAGE ON SCHEMA data TO application_account;
-GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA data TO application_account;
-GRANT SELECT, UPDATE, USAGE ON ALL SEQUENCES IN SCHEMA data TO application_account;
+ALTER DEFAULT privileges IN SCHEMA data GRANT 
+	SELECT, 
+	UPDATE, 
+	INSERT, 
+	DELETE 
+ON TABLES TO application_account;
+ALTER DEFAULT privileges IN SCHEMA data GRANT 
+	SELECT, 
+	UPDATE, 
+	USAGE 
+ON SEQUENCES TO application_account;
 
 COMMIT;
